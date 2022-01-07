91b424beaf5d11ded6167ede170cd9f65065e9f9
Adejoh Sunday <elusivebrown@gmail.com>
2022-01-07 10:50:41 +0100

    7

--- js/drop.js
+++ js/drop.js
@@ -1,63 +1,61 @@
-let swiper = new Swiper(".mySwiper", {
-    
-  loop: true,
-  grabCursor: true,
-  spaceBetween: 48,
+<section class="testimonials">
+            <div class="header-intro">
+              <h3>Testimonies</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="testimonial-container swiper mySwiper">
+                  
+                  <div class="swiper-wrapper">
+                      <div class="testimonial-content swiper-slide">
+                          <div class="testimonial-data">
+                              <div class="testimonial-header">
+                                  <img src="/images/F.jpg" alt="" class="testimonial-img">
+                              <div>
+                                  <h4 class="testimonial_name">Peterside Abdulrahim</h4>
+                                  <p class="testimonial-location">Lagos, Nigeria</p>
+                              </div>
+                              </div>
+                          </div>
+                          <p class="testimonial-description">
+                              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet voluptas est adipisci cumque quaerat quos. Eaque ad optio voluptas provident, ipsam ex culpa repellat at obcaecati officia aliquam harum deleniti.
+                          </p>
+                      </div>
 
-  pagination: {
-    el: ".swiper-pagination",
-    clickable:true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-      568: {
-          slidesPerView: 1,
-          spaceBetween: 20,
+                      <div class="testimonial-content swiper-slide">
+                          <div class="testimonial-data">
+                              <div class="testimonial-header">
+                                  <img src="/images/g.jpg" alt="" class="testimonial-img">
+                              <div>
+                                  <h4 class="testimonial_name">Jenna Udem Lawson</h4>
+                                  <p class="testimony-location">Accra, Ghana</p>
+                              </div>
+                              </div>
+                          </div>
+                          <p class="testimonial-description">
+                              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet voluptas est adipisci cumque quaerat quos. Eaque ad optio voluptas provident, ipsam ex culpa repellat at obcaecati officia aliquam harum deleniti.
+                          </p>
+                      </div>
 
-      },
-      680: {
-        slidesPerView: 2,
-        spaceBetween: 40,
+                      <div class="testimonial-content swiper-slide">
+                          <div class="testimonial-data">
+                              <div class="testimonial-header">
+                                  <img src="/images/h.jpg" alt="" class="testimonial-img">
+                              <div>
+                                  <h4 class="testimonial_name">Cecilia Abraham</h4>
+                                  <p class="testimony-location">Zaria, Nigeria</p>
+                              </div>
+                              </div>
+                          </div>
+                          <p class="testimonial-description">
+                              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet voluptas est adipisci cumque quaerat quos. Eaque ad optio voluptas provident, ipsam ex culpa repellat at obcaecati officia aliquam harum deleniti.
+                          </p>
+                      </div>
 
-    },
-    920: {
-        slidesPerView: 3,
-        spaceBetween: 40,
-
-    },
-    1240: {
-        slidesPerView: 4,
-        spaceBetween: 50,
-
-    }
-  },
-  mousewheel: true,
-  keyboard: true,
-});
-
-
-
-<div class="event-item swiper-slide">
-                                <div class="event-item-date">
-                                    <img src="./images/3.jpg" alt="">
-                                    <div class="event-item-date-item">
-                                        <p>
-                                            Jan
-                                        </p>
-                                        <h4>
-                                            22
-                                        </h4>
-                                    </div>
-                                </div>
-                                
-                                
-                                <div class="event-item-text">
-                                    <h3>The Journey</h3>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, consequuntur!</p>
-                                    <div class="btn btn-a"><a href="">Get Info &#8594</a></div>    
-                                </div>
-                            </div>
+                  </div>
+                  
+                  <div class="swiper-pagination swiper-pagination-testimonial"></div>  
+              </div>
+           
+             
+            
+        </section> 
